d484ef18fd974034a5e02974092f7253656b1667
semih.bagdas <semih.bagdas@gmail.com>
2022-05-26 01:37:31 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Odev1_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Kaç adet pozitif Sayı Gireceksiniz:");
+            int deger = Int32.Parse(Console.ReadLine());
+
+            int[] dizi = new int[deger];
+
+            for(int i=0;i<deger;i++)
+            {
+                Console.Write("{0}. Sayı Giriniz:",i+1);
+                dizi[i]=Int32.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Girdiğiniz Sayılardan Çift Olanları");
+            Console.WriteLine("-------------------");
+            foreach (var item in dizi)
+            {
+                if (item%2==0)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
